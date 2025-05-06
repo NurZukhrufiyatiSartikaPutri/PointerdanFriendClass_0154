e154112793e8d7a0dc97ce2b1ffcc619b8eb5313
NurZukhrufiyatiSartikaPutri <ssartikaaputrii@gmail.com>
2025-05-06 18:32:17 +0700

    membuat class dan akses modifier

---
+++ contoh.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class PersegiPanjang
+{
+    public: // akses modifier
+    int panjang, lebar;
+
+    public:
+    void input()
+    {
+        cout << "Masukkan panjnag : ";
+        cin >> panjang;
+        cout << "Masukkan lebar : ";
+        cin >> lebar;
+    }
+
+    
+};
