391d07c1b301fc9b7705b3095ba771bf0461fb60
NurZukhrufiyatiSartikaPutri <ssartikaaputrii@gmail.com>
2025-05-06 19:32:17 +0700

    membuat class siswa

--- FriendFunctionDua.cpp
+++ FriendFunctionDua.cpp
@@ -13,5 +13,20 @@ class pelajar
 {
 private:
     int nilai;
+
+public: 
+    pelajar(){nilai = 100; }
+    friend void manusia::showNilaiPelajar(pelajar &x);
         
+};
+
+void manusia::showNilaiPelajar(pelajar &x)
+{
+    cout << x.nilai;
 }
+int main()
+{
+    manusia budi;
+    pelajar pbudi;
+    mhs.showNilaiPelajar(pbudi)
+};
